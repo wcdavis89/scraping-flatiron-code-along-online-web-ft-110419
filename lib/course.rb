cd721fc4e9d0d7703dd6e8cb04745ef9c988513c
Whittney Davis <wcdavis89@yahoo.com>
2019-12-08 00:50:14 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,4 +1,11 @@
 class Course
+  attr_accessor :title, :schedule, :description
+  
+  def initialize(title, schedule, description)
+    @title=title
+    @schedule=schedule
+    @description=description
+  end
   
 end
 
