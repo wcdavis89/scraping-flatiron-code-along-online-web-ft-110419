2f5bd7782f5095d4605f34578f4e17a26a29538f
Whittney Davis <wcdavis89@yahoo.com>
2019-12-08 01:00:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
   
   class Scraper
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    
     
   end
   
